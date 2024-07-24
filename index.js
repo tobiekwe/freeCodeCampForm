95cfbdd8d87dc1d78806d284342533800bd4daca
tobiekwe <tgord2002@yahoo.com>
2024-07-24 12:10:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const nameEl = document.getElementById('name');
+const emailEl = document.getElementById('email');
+const ageEl = document.getElementById('age');
+
+const result = document.getElementById('respo')
+const post = document.getElementById("submit");
+
+ 
+post.addEventListener("click", function() {
+  uname = nameEl.value;
+  mail = emailEl.value;
+  uage = ageEl.value;
+
+  document.body.innerHTML=
+  document.getElementById('respo').innerHTML = `
+    Name: ${uname},
+    Email: ${mail},
+    Age: ${uage}`;
+});
+  
+
+
